08c757d030b6cf9ba7d8d3e3848abfef77144539
KerbalsRock <bbishop19@windwardschool.org>
2016-09-07 09:41:09 -0700

    Summary
    Description

---
+++ src/ImJewish.java
@@ -0,0 +1,39 @@
+
+public class ImJewish {
+	public static void firstDayItem() 
+	{
+		System.out.println("A Partridge in a Pear Tree");
+	}
+	public static void secondDayItem() 
+	{
+		System.out.print("Two Turtle Doves\nand");
+	}
+	public static void thirdDayItem() 
+	{
+		System.out.println("Three French Hens");
+	}
+	public static void fourthDayItem() 
+	{
+		System.out.println("Four Calling Birds");
+	}
+	public static void fifthDayItem() 
+	{
+		System.out.println("Five Golden Rings");
+	}
+	public static void sixthDayItem() 
+	{
+		System.out.println("Six Geese a Laying");	
+	}
+	public static void seventhDayItem() 
+	{
+		System.out.println("Seven Swans a Swimming");
+	}
+	public static void eighthDayItem() 
+	{
+		System.out.println("Eight Maids a Milking");
+	}
+	public static void firstDay() 
+	{
+		System.out.println("On the first day of Christmas");
+	}
+}
